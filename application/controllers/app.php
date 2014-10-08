1379d9de1f9cc3fdfd0e57b45153f4eaccfe82b3
imaska1117 <imasaka1117@outlook.com>
2014-10-08 16:19:12 +0800

    test

--- application/controllers/app.php
+++ application/controllers/app.php
@@ -19,7 +19,6 @@ class App extends CI_Controller {
 		$this->load->library('key');
 		$this->load->library('json');
 		$this->load->library('sql');
-
 	}
 
 	/*
@@ -30,6 +29,9 @@ class App extends CI_Controller {
 	public function index() {
 		//每次請求都必須做的檢查,檢查手機ID是否改變,並且整理引導資料
 		$route_data = $this->route_model->index($this->input->post());
+	
+		//依照數字選擇處理
+		$function = array('', 'join_model', 'forget_model', 'alter_model', 'login_model', 'subscribe_model', 'delete_model', 'bill_model', 'problem_model', 'memorandum_model', 'activity_model', 'change_model');
 
 		//依照APP傳來的資料做功能區分,再載入相對應的頁面
 		switch($route_data['control_param']) {
@@ -37,75 +39,64 @@ class App extends CI_Controller {
 				$data['response'] = $route_data['data'];
 				break;
 			case '1':
-				
-				$this->load->library('create');
+			case '5':
 				$this->load->library('push');
 				$this->load->library('sms');
 				$this->load->model('send/push_model');
 				$this->load->model('send/sms_model');
-				$this->load->model('app/join_model');
-				$data['response'] = $this->join_model->index($route_data);
+		
+				if($route_data['control_param'] == 1) {
+					$this->load->library('create');
+					$this->load->model('app/join_model');
+				} else {
+					$this->load->library('transform');
+					$this->load->model('app/subscribe_model');
+				}
 				break;
 			case '2':
 				$this->load->library('create');
 				$this->load->library('email');
 				$this->load->model('send/email_model');
 				$this->load->model('app/forget_model');
-				$data['response'] = $this->forget_model->index($route_data);
 				break;
 			case '3':
 				$this->load->library('create');
 				$this->load->library('sms');
 				$this->load->model('send/sms_model');
 				$this->load->model('app/alter_model');
-				$data['response'] = $this->alter_model->index($route_data);
 				break;
 			case '4':
 				$this->load->model('other/format_model');
 				$this->load->model('app/login_model');
-				$data['response'] = $this->login_model->index($route_data);
-				break;
-			case '5':
-				$this->load->library('push');
-				$this->load->library('sms');
-				$this->load->library('transform');
-				$this->load->model('send/push_model');
-				$this->load->model('send/sms_model');
-				$this->load->model('app/subscribe_model');
-				$data['response'] = $this->subscribe_model->index($route_data);
 				break;
 			case '6':
 				$this->load->model('app/delete_model');
-				$data['response'] = $this->delete_model->index($route_data);
 				break;
 			case '7':
 				$this->load->model('other/format_model');
 				$this->load->model('app/bill_model');
-				$data['response'] = $this->bill_model->index($route_data);
 				break;
 			case '8':
 				$this->load->library('create');
 				$this->load->model('app/problem_model');
-				$data['response'] = $this->problem_model->index($route_data);
 				break;
 			case '9':
 				$this->load->model('app/memorandum_model');
-				$data['response'] = $this->memorandum_model->index($route_data);
 				break;
 			case '10':
 				$this->load->library('sms');
 				$this->load->model('app/activity_model');
-				$data['response'] = $this->activity_model->index($route_data);
 				break;
 			case '11':
 				$this->load->model('app/change_model');
-				$data['response'] = $this->change_model->index($route_data);
 				break;
 			default:
 				$data['response'] = '未授權進入';
 				break;
 		}
-		echo $data['response'];exit();
+		
+		$data['response'] = $this->$function[$route_data['control_param']]->index($route_data);
+
 		//將要回應給APP的結果載入view輸出
 		$this->load->view('app/response.php', $data);
 	}
